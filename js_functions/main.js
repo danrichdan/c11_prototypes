b599030f7be8e2a8a717d8c63f44a69ae51d9939
Dan Riches <driches@ymail.com>
2016-09-28 15:12:31 -0700

    js functions - Dan Riches

---
+++ js_functions/main.js
@@ -0,0 +1,25 @@
+//***************Feature Set 1.1
+function myMessage() {
+    console.log('Woo hoo! My first function.');
+}
+
+//***************Add
+
+function add(x,y) {
+    var result = x + y;
+    console.log(result);
+}
+
+
+//***************Add2
+function add2(x, y) {
+    var total = x + y;
+    return total;
+}
+
+var add2result = add2(10, 36);
+
+function cardFlip(element) {
+    $(element).hide();
+}
+
